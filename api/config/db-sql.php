1ef31a470b16ff040af116397c4ac9956c5bbff5
niallone <hola@niall.one>
2019-06-21 10:26:13 +1000

    Tidy

--- api/config/db-sql.php
+++ api/config/db-sql.php
@@ -1,6 +1,6 @@
 <?php
-$dsn = 'mysql:unix_socket=/var/run/mysqld/mysqld.sock;host=db;dbname=intadoc_sql;charset=utf8';
-$connection = new \PDO($dsn, 'int_db_user', 'FYf2IpgopGVfwvsmjdI6vthbKB5A3G0jgt8AYHTS9y6sYz9Pvc7Gg');
+$dsn = 'mysql:unix_socket=/var/run/mysqld/mysqld.sock;host=db;dbname=changeme;charset=utf8';
+$connection = new \PDO($dsn, 'changeme', 'changeme');
 // throw exceptions, when SQL error is caused
 $connection->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
 // prevent emulation of prepared statements
