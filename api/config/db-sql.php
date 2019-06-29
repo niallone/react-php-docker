f4d0bfbba9ce60850ed2ac866eee8cd65d837579
niallone <hola@niall.one>
2019-06-29 12:05:23 +1000

    Mounting rethinkdb data externally

--- api/config/db-sql.php
+++
@@ -1,7 +0,0 @@
-<?php
-$dsn = 'mysql:unix_socket=/var/run/mysqld/mysqld.sock;host=db;dbname=changeme;charset=utf8';
-$connection = new \PDO($dsn, 'changeme', 'changeme');
-// throw exceptions, when SQL error is caused
-$connection->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-// prevent emulation of prepared statements
-$connection->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
